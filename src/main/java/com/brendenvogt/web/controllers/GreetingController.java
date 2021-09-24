e373fd2068165a6aa15ffdebc7fafaf8678d4a15
Brenden Vogt <bvogtdev@gmail.com>
2021-09-24 02:00:38 -0700

    Refactored endpoints into their own controller classes

---
+++ src/main/java/com/brendenvogt/web/controllers/GreetingController.java
@@ -0,0 +1,16 @@
+package com.brendenvogt.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        // return json object
+        return String.format("Hello %s!", name);
+    }
+
+}
---
+++ src/main/java/com/brendenvogt/web/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package com.brendenvogt.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Example Usage
+ * localhost:8080/hello?name=Brenden
+ * returns
+ * Hello Brenden!
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
