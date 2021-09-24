08d51245273d326cd54ec77e234a04bbde4c3de8
Brenden Vogt <bvogtdev@gmail.com>
2021-09-24 03:29:32 -0700

    Added files for PersonRepo Person model and CommandLineApplication for testing the MongoDB integration

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Simple Java Spring Web project as a playground
 ```
 
 ``` 
-curl -X GET "localhost:8080/hello?name=Brenden"
+curl -X GET "localhost:8080/hello?firstName=Brenden"
 # Hello Brenden!%
 ```
 
@@ -25,6 +25,43 @@ curl -X GET "localhost:8080/hello?name=Brenden"
 java -jar target/<JAR_FILE>.jar
 ```
 
+### Start Mongo
+- start command
+```
+docker run -d -p 27017:27017 --name mongodb  mongo
+```
+- stop command
+```
+docker stop mongodb
+docker rm mongodb
+```
+- or expand to a docker compose yaml in the future
+``` yaml
+# Use root/example as user/password credentials
+version: '3.1'
+
+services:
+
+  mongo:
+    image: mongo
+    restart: always
+    environment:
+      MONGO_INITDB_ROOT_USERNAME: root
+      MONGO_INITDB_ROOT_PASSWORD: example
+
+  mongo-express:
+    image: mongo-express
+    restart: always
+    ports:
+      - 8081:8081
+    environment:
+      ME_CONFIG_MONGODB_ADMINUSERNAME: root
+      ME_CONFIG_MONGODB_ADMINPASSWORD: example
+      ME_CONFIG_MONGODB_URL: mongodb://root:example@mongo:27017/
+```
+
+
 ## References
 - https://spring.io/quickstart
 - https://spring.io/guides/gs/rest-service/
+- https://hub.docker.com/_/mongo
---
+++ src/main/java/com/brendenvogt/web/CommandLineApplication.java
@@ -0,0 +1,37 @@
+package com.brendenvogt.web;
+
+import com.brendenvogt.web.models.Person;
+import com.brendenvogt.web.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandLineApplication implements CommandLineRunner {
+
+    @Autowired
+    private PersonRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandLineApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        repository.deleteAll();
+
+        // save a couple of customers
+        repository.save(new Person("Alice", "Smith"));
+        repository.save(new Person("Bob", "Smith"));
+
+        // print out customers by last name smith
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Person person : repository.findByLastName("Smith")) {
+            System.out.println(person);
+            System.out.printf("Person first name: %s, last name: %s%n", person.getFirstName(), person.getLastName());
+        }
+    }
+}
